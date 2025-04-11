72182bcb4dbfdaecc944665bf13aa1bccd7683a4
vishalpokuri <vishal.pokuri@gmail.com>
2025-04-12 01:01:59 +0530

    Tracking #2

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
+// src/main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import { PostHogProvider } from "posthog-js/react";
 
 const options = {
-  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
 };
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <PostHogProvider
-      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY!}
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
       options={options}
     >
       <App />
